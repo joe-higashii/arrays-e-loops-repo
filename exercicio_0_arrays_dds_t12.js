864294054a887acb41a2dc2fe8cfe7c5ccfcb0c0
Joe <joedsonfera@hotmail.com>
2023-04-12 18:35:10 -0300

    fifth commit

---
+++ exercicio_0_arrays_dds_t12.js
@@ -0,0 +1,33 @@
+// Crie um array com 5 nomes de países
+const nomesDePaises = ["brasil", "alemanha", "holanda", "suecia", "chile"];
+
+// Adicione um país no fim da lista
+nomesDePaises.push("Senegal");
+
+// Imprima a lista na tela
+// console.log(nomesDePaises);
+
+// Remova um país do fim da lista
+nomesDePaises.pop();
+// console.log(nomesDePaises);
+
+// Adicione um país no início da lista
+nomesDePaises.unshift("Argentina");
+
+// Imprima a lista na tela
+// console.log(nomesDePaises);
+
+// Remova um país do início da lista
+nomesDePaises.shift();
+
+// Imprima a lista na tela
+console.log(nomesDePaises);
+
+// Imprima o último país da lista na tela
+console.log(nomesDePaises[4]);
+
+// Imprima o segundo país da lista na tela
+console.log(nomesDePaises[1]);
+
+// Imprima o país de índice 2 na tela
+console.log(nomesDePaises[2]);
---
+++ while.js
@@ -0,0 +1,9 @@
+const nomesDeEstudantes = ["jose", "joao", "maria", "joana", "andre"];
+
+let indice = 0;
+
+// sempre vai executar o que estiver dentro do array se a condição for verdadeira
+while (indice < nomesDeEstudantes.length) { //enquanto
+    console.log(nomesDeEstudantes[indice]);
+    indice++;
+}
