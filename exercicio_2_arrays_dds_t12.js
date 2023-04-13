8e803c3950c9b425aacbc91ad7a2f7eb282aa7f4
Joe <joedsonfera@hotmail.com>
2023-04-13 13:40:33 -0300

    first commit of the day

---
+++ exercicio_2_arrays_dds_t12.js
@@ -0,0 +1,11 @@
+// criando um array de 5 numeros
+
+const nomeDeVilas = ["Amegakure", "Konohagakure", "Sunagakure", "Otogakure", "Kirigakure"]
+
+let indice = 0;
+
+// enquanto o indice for menor que o tamanho do array
+while (indice < nomeDeVilas.length) {   // executar essa linha
+    console.log(nomeDeVilas[indice]);
+    indice++;
+}
